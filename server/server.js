927c44545c5071ed33f8d9c7c6555419d0d7483d
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-07 21:51:23 -0700

    server js set up for apollo graphql server

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
 const { ApolloServer } = require('apollo-server-express');
 
+const path = require('path');
+const db = require('./config/connection');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
 });
 
-server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,11 +24,22 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-db.once('open', () => {
+//new instance of apollo server and schema
+const startApollo = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({app});
+
+  db.once('open', () => {
   app.listen(PORT, () => {
         console.log(`🌍 API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
-});
+    })
+  })
+};
+
+//funciton call to start server
+startApollo(typeDefs, resolvers);
