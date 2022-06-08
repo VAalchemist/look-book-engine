072e6d9fbb97fccdc06d92f4f121ef89c26fd7ab
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-07 21:37:49 -0700

    resolvers done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,23 +18,57 @@ const resolvers = {
 
   Mutation: {
     addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return {token, user};
     },
 
     login: async (parent, {email, password}) => {
+            const user = await User.findOne({ email });
 
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
     },
 
-    saveBook: async (parent, args, context) => {
+    saveBook: async (parent, {bookData}, context) => {
+        if (context.user) {
+          const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+          );
 
+          return updateUser;
+        } 
+
+        throw new AuthenticationError('Must be logged in!');
     },
 
-    removeBook: async (parent, args, context) => {
+    removeBook: async (parent, {bookId}, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+        );
 
-    }    
-  }
+        return updatedUser;
+      }
 
-
+      throw new AuthenticationError('Must be logged in!');
+    },    
+  },
 };
 
 
