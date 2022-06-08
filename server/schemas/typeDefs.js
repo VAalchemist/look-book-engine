548134531fe13746f46fbd8997b476559f1dbeb6
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-07 21:28:11 -0700

    all types defined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,19 +11,37 @@ type User {
 }
 
 type Book {
-  
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+}
+
+type BookInput {
+    authors: [String]
+    description: String!
+    bookId: ID!
+    image: String
+    link: String
+    title: String!
 }
 
 type Query {
-  
+  me: User
 }
 
 type Mutation {
-  
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(bookData: BookInput!): User
+  removeBook(bookId: ID!): User
 }
 
 type Auth {
-  
+  token: ID!
+  user: User
 }
 `;
 
