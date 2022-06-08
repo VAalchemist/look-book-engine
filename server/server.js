3d3afcd9bfdb84ded22dfeb5b18fb632339767fe
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-08 02:05:43 -0700

    backend finally working!!

--- server/server.js
+++ server/server.js
@@ -19,15 +19,15 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 //new instance of apollo server and schema
 const startApollo = async (typeDefs, resolvers) => {
   await server.start();
